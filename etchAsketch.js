b668bd33923c7830909164542c05d40f1f53eba4
Essohanam <essoh.alou@gmail.com>
2022-04-13 09:55:03 +0000

    event listeners

--- etchAsketch.js
+++ etchAsketch.js
@@ -1,6 +1,19 @@
 let squaresContainer = document.querySelector(".squaresContainer");
+let buttonToDraw = document.querySelector(".buttonToDraw");
+let buttonToClear = document.querySelector(".buttonToClear");
+let erase = document.querySelector(".erase");
+let rainbow = document.querySelector(".rainbow");
+let colorContainer = document.querySelector(".colorContainer");
+let userScreenContainer = document.querySelector(".userScreenContainer");
+let squaresRange = document.querySelector("#squaresRange");
+let numbers = document.querySelector(".numbers");
 
-let numberOfSquares = 15;
+
+
+let mouseDown = false;
+let colorPicker = colorContainer.value;
+let color = colorPicker;
+let numberOfSquares = squaresRange.value;
 let squaresContainerWidth = 410;
 let squaresContainerHeight = 410;
 let squaresContainerArea = squaresContainerHeight * squaresContainerWidth;
@@ -20,4 +33,77 @@ function drawSquares(numberOfSquares, surfaceToUse) {
     surfaceToUse.style.display = "grid";
     surfaceToUse.style.gridTemplateColumns = `repeat(${numberOfSquares},${squareLength}px)`;
 }
-drawSquares(numberOfSquares, squaresContainer);
+
+// a fonction to blank the screen
+function clearDiv() {
+    let nsquare = numberOfSquares * numberOfSquares;
+    for (let i = 1; i <= nsquare; i++) {
+        let square = document.querySelector(".square");
+        square.remove();
+    }
+}
+// function to chose mode 
+
+
+
+// adding events to buttons
+
+squaresRange.addEventListener('input', () => {
+    numberOfSquares = squaresRange.value;
+    numbers.textContent = `${numberOfSquares}x${numberOfSquares}`;
+});
+buttonToDraw.addEventListener("click", () => {
+
+    numberOfSquares = squaresRange.value;
+    drawSquares(numberOfSquares, squaresContainer);
+    let squaresNodeList = document.querySelectorAll('.square');
+    let squaresNodeListLength = squaresNodeList.length;
+    for (let i = 0; i < squaresNodeListLength; i++) {
+        squaresNodeList[i].addEventListener('click', () => {
+
+            squaresNodeList[i].style.backgroundColor =
+                color;
+
+        })
+
+        squaresNodeList[i].onmousemove = () => {
+            squaresContainer.onmousedown = () => { mouseDown = true };
+            squaresContainer.onmouseup = () => { mouseDown = false };
+            if (!mouseDown) return;
+            color = color;
+            squaresNodeList[i].style.backgroundColor =
+                color;
+        }
+    }
+
+
+});
+
+
+buttonToClear.addEventListener('click', () =>
+    clearDiv());
+
+
+colorContainer.addEventListener('input', () => {
+    colorPicker = colorContainer.value;
+    color = colorPicker;
+    userScreenContainer.style.setProperty("--picked-color", `${color}`);
+
+
+});
+
+colorContainer.addEventListener('change', () => {
+    colorPicker = colorContainer.value;
+    color = colorPicker;
+    userScreenContainer.style.setProperty("--picked-color", `${color}`);
+
+
+});
+
+rainbow.onclick = () => {
+    let amountOfRed = Math.floor(Math.random() * 255 + 1);
+    let amountOfGreen = Math.floor(Math.random() * 255 + 1);
+    let amountOfBlue = Math.floor(Math.random() * 255 + 1);
+    color = `rgb(${amountOfRed}, ${amountOfGreen}, ${amountOfBlue})`;
+};
+erase.onmousedown = () => { color = '#fff' };
